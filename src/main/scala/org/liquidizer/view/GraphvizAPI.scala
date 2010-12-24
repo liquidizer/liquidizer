9bfbadc4d682e69d4170fb9ef1bd497ea2d586d5
Stefan Dirnstorfer <stefan@dadim.de>
2010-12-24 14:57:27 +0100

    mark current user in delegation graph

--- src/main/scala/org/liquidizer/view/GraphvizAPI.scala
+++ src/main/scala/org/liquidizer/view/GraphvizAPI.scala
@@ -6,6 +6,7 @@ import java.io._
 import java.util._
 
 import net.liftweb.mapper._
+import net.liftweb.common._
 
 import org.liquidizer.lib._
 import org.liquidizer.model._
@@ -35,34 +36,23 @@ class GraphvizAPI {
       case VotableQuery(_) => 
 	label= VoteCounter.getWeight(edge.from, edge.to)
         factor= label * label * 0.5
-      case VotableUser(_) => 
+      case VotableUser(user) => 
 	factor= VoteCounter.getCumulativeWeight(edge.from, edge.to)
         label= factor * VoteCounter.getDelegationInflow(edge.from)
+        // always show the current User if adjacent
+        User.currentUser match {
+	  case Full(user) if user==edge.from || VotableUser(user)==edge.to => 
+	    factor=1.0
+	    if (!options.contains(VotableUser(user)))
+		options.put(VotableUser(user), 
+			    "style=\"filled\" fillcolor=\"#FF9933\"")
+	  case _ => 
+	}
     }
     options.put(edge, 
 		"color=\""+(if (factor>=0) "black" else "red")+"\" " +
 		"label=\""+formatDouble(label)+"\"")
     factor
-  }
-
-  val yellow= SVGUtil.parseColor("#f4e514")
-  val red= SVGUtil.parseColor("#CC0000")
-  val white= SVGUtil.parseColor("#FFFFFF")
-
-  def blend(color1: (Int,Int,Int), color2: (Int,Int,Int), w:Double) =
-    ((color1._1 * w + color2._1 * (1-w)).toInt,
-     (color1._2 * w + color2._2 * (1-w)).toInt,
-     (color1._3 * w + color2._3 * (1-w)).toInt)
-
-  def setOptions(node : Votable, weight : Double) = {
-    val color=
-      if (weight>0)
-	blend(yellow, white, weight min 1.0)
-      else
-      	blend(red, white, (-weight) min 1.0)
-    options.put(node, 
-		"style=\"filled\"" +
-		"color=\""+SVGUtil.formatColor(color._1, color._2, color._3)+"\"")
   }
 
   def process(node : Votable, weight : Double) = {
