8a7d65ee6713d8a0d112f9eb0430eb96145346c1
Stefan Dirnstorfer <stefan@dadim.de>
2011-02-13 19:39:25 +0100

    direct link to nominee for speedup

--- src/main/scala/org/liquidizer/lib/VoteCounter.scala
+++ src/main/scala/org/liquidizer/lib/VoteCounter.scala
@@ -81,6 +81,9 @@ object VoteCounter {
   }
   
   def init() = {
+    User.findAll.foreach { u => if (!u.nominee.defined_?) { u.createNominee; u.save }}
+    Query.findAll.foreach { u => if (!u.nominee.defined_?) { u.createNominee; u.save }}
+
     mapper.start
     Vote.findAll(OrderBy(Vote.date, Ascending)).foreach {
       vote =>
